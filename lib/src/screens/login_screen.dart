78f934568b370572a16ed5f905c5d22a637926fc
ZamanakAndroid <a.raghibdost@zamanak.ir>
2019-01-22 15:15:37 +0330

    completed

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../mixin/validation_mixin.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> with ValidationMixin{
+  final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('Submit'),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Time to post $email and $password to API');
+        }
+      },
+    );
+  }
+}
